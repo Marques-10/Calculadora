e4550b1e090e522cdc639d41b4edb6db37692e16
Marques Lucas <xjmarques@hotmail.com>
2020-07-03 22:12:15 -0300

    Updates

---
+++ precache-manifest.1e6d69fdb603f9cb59c71f9cd17d6100.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbe56f166646733bfdb90ed2d9a15996",
+    "url": "/Calculadora/index.html"
+  },
+  {
+    "revision": "ec3e66c2062383c28144",
+    "url": "/Calculadora/static/css/main.abd7d9c5.chunk.css"
+  },
+  {
+    "revision": "3e1495fd4737f2b32d62",
+    "url": "/Calculadora/static/js/2.fd711a93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculadora/static/js/2.fd711a93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec3e66c2062383c28144",
+    "url": "/Calculadora/static/js/main.61cfe456.chunk.js"
+  },
+  {
+    "revision": "6b785d3e335534463bce",
+    "url": "/Calculadora/static/js/runtime-main.ab6cee8b.js"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/Calculadora/static/media/Roboto-Regular.11eabca2.ttf"
+  }
+]);
